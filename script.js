1af8533ca25aeb37bd0a729122e77033ee641f91
Pranesh S <124971841+Praneshcandy@users.noreply.github.com>
2023-10-28 01:27:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// var request = new XMLHttpRequest();
+// request.open("GET","https://restcountries.com/v2/all");
+// request.send();
+// request.onload = function(){
+//     var result = JSON.parse(request.response);
+//   var res = result.filter((ele)=>ele.dollars==="currency")
+//   console.log(res)
+// }
+function foo() {
+  var res= document.getElementById("first").value;
+   console.log(res);
+   var res1= document.getElementById("middle").value;
+   console.log(res1);
+   var res2= document.getElementById("last").value;
+   console.log(res2);
+   var res3= document.getElementById("email").value;
+   console.log(res3);
+   var res4= document.getElementById("password").value;
+   console.log(res4);
+}
